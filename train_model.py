ad5823401de4ab1571536c3ce9f33f2732f4d222
vennuharshitha25-coder <vennuharshitha25@gmail.com>
2026-04-19 18:41:12 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,14 +5,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
 
-# Load dataset
 df = pd.read_csv("dataset/uberdatasett.csv")
-
-# Remove datetime column if exists
 if "pickup_datetime" in df.columns:
     df.drop("pickup_datetime", axis=1, inplace=True)
 
-# Encode ALL text columns
 encoders = {}
 
 for col in df.select_dtypes(include=["object", "string"]).columns:
@@ -20,18 +16,14 @@ for col in df.select_dtypes(include=["object", "string"]).columns:
     df[col] = le.fit_transform(df[col].astype(str))
     encoders[col] = le
 
-# Target column
 y = df["fare_amount"]
 
-# Features
 X = df.drop("fare_amount", axis=1)
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Model
 model = RandomForestRegressor(
     n_estimators=200,
     random_state=42
@@ -39,15 +31,15 @@ model = RandomForestRegressor(
 
 model.fit(X_train, y_train)
 
-# Predict
 pred = model.predict(X_test)
 
 print("R2 Score:", r2_score(y_test, pred))
 print("MAE:", mean_absolute_error(y_test, pred))
 
-# Save files
 joblib.dump(model, "model.pkl")
 joblib.dump(encoders, "encoders.pkl")
 joblib.dump(list(X.columns), "features.pkl")
 
 print("model.pkl created successfully")
+print("encoders.pkl created successfully")
+print("features.pkl created successfully")
